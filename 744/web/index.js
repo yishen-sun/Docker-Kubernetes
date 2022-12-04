fe690631d3f724636add762733acd921029676ca
Yishen Sun <sunyishen@MacBook-Pro-14-Ethan.local>
2022-12-04 16:55:45 -0600

    forshichun

--- 744/web/index.js
+++ 744/web/index.js
@@ -1,10 +1,9 @@
-
-labels ={
+labels = {
   0: "setosa",
   1: "versicolor",
-  2: "virginica"
-}
-url = 'http://localhost:8000/api'
+  2: "virginica",
+};
+url = "http://localhost:8000/api";
 
 let vector = window.prompt("Please enter the vector, eg: 5.8 4.0 1.2 0.2");
 vector = [5.8, 4.0, 1.2, 0.2];
@@ -14,13 +13,26 @@ vector = [5.8, 4.0, 1.2, 0.2];
 var xhr = new XMLHttpRequest();
 xhr.open("POST", url, true);
 xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-xhr.send(JSON.stringify({
-  'feature': vector
-}));
-xhr.onload = function() {
-  console.log("HELLO")
+xhr.send(
+  JSON.stringify({
+    feature: vector,
+  })
+);
+xhr.onload = function () {
+  console.log("HELLO");
   console.log(this.responseText);
   var data = JSON.parse(this.responseText);
   console.log(data);
-}
+};
 
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector(
+      "#display-image"
+    ).style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
